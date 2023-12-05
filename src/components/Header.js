018720fe8ecfb4bea0e555df607be2aa52aa43d8
hwahyeon <hwahyeon@knou.ac.kr>
2023-12-05 22:09:07 +0900

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/painting">Painting</Link></li>
+          <li><Link to="/books">Books</Link></li>
+          <li><Link to="/info">Info</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
